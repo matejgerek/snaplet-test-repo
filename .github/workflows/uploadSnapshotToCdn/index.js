178af5a4eb9f05b32481946c5cb0d589c6c18949
matejgerek <maftvej@gmail.com>
2023-09-20 20:12:12 +0200

    Change the path to workflows

--- .github/workflows/uploadSnapshotToCdn/index.js
+++ .github/workflows/uploadSnapshotToCdn/index.js
@@ -43,7 +43,7 @@ const uploadFile = async (localFilePath) => {
 
 const main = async () => {
   console.log('uploading')
-  const directory = await fs.promises.readdir('./.github', { withFileTypes: true })
+  const directory = await fs.promises.readdir('./.github/workflows', { withFileTypes: true })
   console.log(directory)
 }
 
